db654cec1d164b2242f6773aea8451ea8d8b7d52
ErfanGhohroudi <erfanghohroodi1998@gmail.com>
2025-05-21 12:08:56 +0330

    final

--- lib/components/signaturepad.dart
+++ lib/components/signaturepad.dart
@@ -4,49 +4,70 @@ import 'package:u/utilities.dart';
 
 class SignaturePad extends StatelessWidget {
   SignaturePad({
-    super.key,
     required this.onSave,
     this.saveButtonText = "Save",
     this.clearButtonText = "Clear",
+    this.pixelRatio = 1.0,
+    super.key,
   });
 
   final GlobalKey<SfSignaturePadState> signatureGlobalKey = GlobalKey();
-  final Function(FileData) onSave;
+  final Rx<bool> isNotEmpty = false.obs;
+  final Function(FileData fileData, bool isEmpty) onSave;
   final String saveButtonText;
   final String clearButtonText;
+  final double pixelRatio;
 
   @override
   Widget build(BuildContext context) => Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            margin: EdgeInsets.all(10),
+            width: 300,
+            height: 300,
             decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
             child: SfSignaturePad(
               key: signatureGlobalKey,
-              backgroundColor: Colors.white,
+              backgroundColor: Colors.grey[100],
               strokeColor: Colors.black,
-              minimumStrokeWidth: 1,
-              maximumStrokeWidth: 4,
+              minimumStrokeWidth: 1.5,
+              maximumStrokeWidth: 1.5,
+              onDrawEnd: () {
+                isNotEmpty(true);
+              },
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            spacing: 10,
             children: <Widget>[
-              TextButton(onPressed: handleSaveButtonPressed, child: Text(saveButtonText)),
-              TextButton(onPressed: () => signatureGlobalKey.currentState!.clear(), child: Text(clearButtonText)),
+              UElevatedButton(
+                title: clearButtonText,
+                backgroundColor: context.theme.hintColor,
+                onTap: () {
+                  signatureGlobalKey.currentState!.clear();
+                  isNotEmpty(false);
+                },
+              ).expanded(),
+              Obx(
+                () => UElevatedButton(
+                  title: saveButtonText,
+                  backgroundColor: isNotEmpty.value ? null : context.theme.dividerColor,
+                  onTap: handleSaveButtonPressed,
+                ),
+              ).expanded(),
             ],
           )
         ],
       );
 
   void handleSaveButtonPressed() async {
-    final ui.Image data = await signatureGlobalKey.currentState!.toImage(pixelRatio: 3.0);
+    final ui.Image data = await signatureGlobalKey.currentState!.toImage(pixelRatio: pixelRatio);
     final ByteData? bytes = await data.toByteData(format: ui.ImageByteFormat.png);
     final Uint8List byte = bytes!.buffer.asUint8List();
     File file = await UFile.writeToFile(byte);
-    onSave(FileData(path: file.path, bytes: byte, extension: "png"));
+    onSave(FileData(path: file.path, bytes: byte, extension: "png"), !isNotEmpty.value);
   }
 }
