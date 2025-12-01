6d95f25e8eba01478fbcaed5e547b26b02bd69c6
ErfanGhohroudi <erfanghohroodi1998@gmail.com>
2025-12-01 15:06:30 +0330

    final

--- lib/components/link_previewer.dart
+++ lib/components/link_previewer.dart
@@ -7,25 +7,30 @@ class ULinkPreviewer extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) => AnyLinkPreview(
-        link: link,
-        displayDirection: UIDirection.uiDirectionHorizontal,
-        bodyMaxLines: 10,
-        titleStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
-        errorWidget: Text(
-          link,
-          style: TextStyle(
-            fontSize: 12,
-            color: Theme.of(navigatorKey.currentContext!).colorScheme.brightness == Brightness.dark ? Colors.white : Colors.black,
-          ),
-        ).onTap(() => ULaunch.launchURL(link)),
-        bodyStyle: const TextStyle(
+    link: link,
+    displayDirection: UIDirection.uiDirectionHorizontal,
+    bodyMaxLines: 10,
+    titleStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+    errorWidget: GestureDetector(
+      onTapDown: (final details) {
+        ULaunch.launchURL(link);
+      },
+      child: Text(
+        link,
+        style: TextStyle(
           fontSize: 12,
-          color: Colors.black,
+          color: Theme.of(navigatorKey.currentContext!).colorScheme.brightness == Brightness.dark ? Colors.white : Colors.black,
         ),
-        cache: const Duration(days: 7),
-        borderRadius: 12,
-        backgroundColor: Colors.white,
-        urlLaunchMode: LaunchMode.externalApplication,
-        boxShadow: const <BoxShadow>[BoxShadow(blurRadius: 3)],
-      );
+      ),
+    ),
+    bodyStyle: const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+    ),
+    cache: const Duration(days: 7),
+    borderRadius: 12,
+    backgroundColor: Colors.white,
+    urlLaunchMode: LaunchMode.externalApplication,
+    boxShadow: const <BoxShadow>[BoxShadow(blurRadius: 3)],
+  );
 }
