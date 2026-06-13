1845956df59d305995daab1909709573553f17c2
ErfanGhohroudi <erfanghohroodi1998@gmail.com>
2026-06-13 10:57:38 +0330

    final update UOtpField

--- lib/components/otp_field.dart
+++ lib/components/otp_field.dart
@@ -2,69 +2,81 @@ import 'package:u/utilities.dart';
 
 class UOtpField extends StatelessWidget {
   const UOtpField({
+    super.key,
+    required this.pinController,
     this.length = 4,
     this.autoFocus = false,
-    this.controller,
-    this.mainAxisAlignment = MainAxisAlignment.spaceBetween,
-    this.hintCharacter,
     this.onCompleted,
-    this.borderRadius = 8,
-    this.shape = PinCodeFieldShape.box,
-    this.fieldOuterPadding,
-    this.fieldHeight = 64,
+    this.validator,
     this.fieldWidth = 60,
+    this.fieldHeight = 64,
+    this.borderRadius = 8,
     this.fillColor,
     this.borderColor,
+    this.activeColor,
     this.cursorColor,
+    this.textStyle,
+    this.hintStyle,
     this.onTap,
-    this.validator,
-    super.key,
+    this.theme,
+    this.keyboardType,
+    this.hintCharacter,
   });
 
+  final PinInputController pinController;
   final int length;
   final bool autoFocus;
-  final TextEditingController? controller;
-  final MainAxisAlignment mainAxisAlignment;
-  final String? hintCharacter;
-  final void Function(String)? onCompleted;
-  final double borderRadius;
-  final PinCodeFieldShape shape;
-  final EdgeInsetsGeometry? fieldOuterPadding;
-  final double fieldHeight;
   final double fieldWidth;
+  final double fieldHeight;
+  final double borderRadius;
   final Color? fillColor;
   final Color? borderColor;
+  final Color? activeColor;
   final Color? cursorColor;
-  final Function? onTap;
+  final TextStyle? textStyle;
+  final TextStyle? hintStyle;
+  final VoidCallback? onTap;
+  final MaterialPinTheme? theme;
+  final TextInputType? keyboardType;
+  final String? hintCharacter;
   final String? Function(String?)? validator;
+  final ValueChanged<String>? onCompleted;
 
   @override
-  Widget build(BuildContext context) => PinCodeTextField(
-        controller: controller,
-        appContext: navigatorKey.currentContext!,
-        length: length,
-        cursorColor: cursorColor,
-        onTap: onTap,
-        autoFocus: autoFocus,
-        mainAxisAlignment: mainAxisAlignment,
-        hintCharacter: hintCharacter,
-        validator: validator,
-        pinTheme: PinTheme(
-          shape: shape,
-          fieldOuterPadding: fieldOuterPadding,
-          borderRadius: BorderRadius.circular(borderRadius),
-          fieldHeight: fieldHeight,
-          fieldWidth: fieldWidth,
-          activeFillColor: fillColor,
-          inactiveFillColor: fillColor,
-          selectedFillColor: fillColor,
-          inactiveColor: borderColor,
-          selectedColor: borderColor,
-          activeColor: borderColor,
-        ),
-        enableActiveFill: true,
-        keyboardType: TextInputType.number,
-        onCompleted: onCompleted,
-        onChanged: (final _) {},
-      ).ltr();
+  Widget build(BuildContext context) {
+    return MaterialPinFormField(
+      length: length,
+      pinController: pinController,
+      autoFocus: autoFocus,
+      enableAutofill: true,
+      autofillHints: const [
+        AutofillHints.oneTimeCode,
+      ],
+      hintCharacter: hintCharacter,
+      keyboardType: keyboardType ?? const TextInputType.numberWithOptions(signed: false, decimal: false),
+      onTapOutside: (event) => FocusManager.instance.primaryFocus!.unfocus(),
+      validator: validator,
+      onTap: onTap,
+      onCompleted: onCompleted,
+      theme:
+      theme ??
+          MaterialPinTheme(
+            shape: MaterialPinShape.outlined,
+            cellSize: Size(
+              fieldWidth,
+              fieldHeight,
+            ),
+            borderRadius: BorderRadius.circular(
+              borderRadius,
+            ),
+            borderColor: borderColor,
+            focusedBorderColor: activeColor,
+            fillColor: fillColor,
+            focusedFillColor: fillColor,
+            filledFillColor: fillColor,
+            textStyle: textStyle,
+            cursorColor: cursorColor,
+          ),
+    ).ltr();
+  }
 }
